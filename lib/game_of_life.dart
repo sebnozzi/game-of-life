bbd37485928c90b3c0ed101f8fe9789af76db1ba
Sebastian Nozzi <sebnozzi@gmail.com>
2015-11-11 20:11:35 +0100

    Implemented some rules

--- lib/game_of_life.dart
+++ lib/game_of_life.dart
@@ -4,12 +4,77 @@
 /// The game_of_life library.
 library game_of_life;
 
+class Position {
+
+  int x;
+  int y;
+
+  Position(this.x, this.y) {
+  }
+
+  bool isAt(int x, int y) {
+    return this.x == x && this.y == y;
+  }
+
+  Position add(Position other) {
+    return new Position(this.x + other.x, this.y + other.y);
+  }
+
+}
+
 class Board {
+
+  Set<Position> _cells = new Set();
 
   bool isEmpty() => true;
 
   Board nextState() {
-    return new Board();
+    Board nextBoard = new Board();
+
+    // iterate over cells
+    // count neighbours for each cell
+    // if it has 2, then it lives on
+
+    for(Position position in _cells.toList()) {
+      int neighbourCount = _countNeighboursAt(position);
+      if(neighbourCount == 2) {
+        nextBoard.addCellAt(position.x, position.y);
+      }
+    }
+
+    return nextBoard;
   }
+
+  void addCellAt(int x, int y) {
+    _cells.add(new Position(x, y));
+  }
+
+  bool hasCellAt(int x, int y) {
+    return _cells.any((p) => p.isAt(x, y));
+  }
+
+  int _countNeighboursAt(Position pos) {
+    List<Position> neighbourOffsets = [
+      new Position(-1, -1),
+      new Position(0, -1),
+      new Position(1, -1),
+      new Position(-1, 0),
+      new Position(1, 0),
+      new Position(-1, 1),
+      new Position(0, 1),
+      new Position(1, 1)
+    ];
+    int count = 0;
+    for(Position offset in neighbourOffsets){
+      Position neighbourPos = pos.add(offset);
+      if(hasCellAt(neighbourPos.x, neighbourPos.y)) {
+        count++;
+      }
+    }
+    return count;
+  }
+
 }
+
+
 
--- test/game_of_life_test.dart
+++ test/game_of_life_test.dart
@@ -11,8 +11,56 @@ Board makeEmptyBoard() {
   return board;
 }
 
+Board makeBoardWithCellAt_2_2() {
+  Board board = new Board();
+  board.addCellAt(2, 2);
+  return board;
+}
+
 void main() {
 
+
+
+  group('initially', () {
+
+    test('a board is empty', () {
+      var board = makeEmptyBoard();
+      expect(board.isEmpty(), isTrue);
+    });
+
+  });
+
+  test('a board can add live cells', () {
+    Board board = new Board();
+    board.addCellAt(2, 2);
+  });
+
+  group('asking for cells:', () {
+    test('should find present cell', () {
+      Board board = makeBoardWithCellAt_2_2();
+      expect(board.hasCellAt(2, 2), isTrue);
+    });
+    test('should NOT find absent cell', () {
+      Board board = makeBoardWithCellAt_2_2();
+      expect(board.hasCellAt(3, 5), isFalse);
+    });
+  });
+
+  group('next-state', () {
+
+    test('of an empty board is an empty board', () {
+      var board = makeEmptyBoard();
+      var nextBoard = board.nextState();
+      expect(nextBoard.isEmpty(), isTrue);
+    });
+
+    test('of a board with one cell is an empty board (rule 1)', () {
+      var board = makeBoardWithCellAt_2_2();
+      var nextBoard = board.nextState();
+      expect(nextBoard.isEmpty(), isTrue);
+    });
+
+  });
 
   /*
   *  Rules:
@@ -23,20 +71,78 @@ void main() {
   *  4. Any dead cell with exactly three live neighbours becomes a live cell.
   *
   * */
+  group('Rule', () {
 
-  test('a board can be instantiated', () {
-    new Board();
-  });
+    group('1. Any live cell with fewer than two live neighbours dies:', () {
 
-  test('a board is initially empty', () {
-    var board = makeEmptyBoard();
-    expect(board.isEmpty(), isTrue);
-  });
+      test('Lone cell', () {
+        var board = new Board();
+        board.addCellAt(2,2);
+        var nextBoard = board.nextState();
+        expect(nextBoard.hasCellAt(2,2), isFalse);
+      });
 
-  test('the next state of an empty board is an empty board', () {
-    var board = makeEmptyBoard();
-    var nextBoard = board.nextState();
-    expect(nextBoard.isEmpty(), isTrue);
+      test('Only one neighbour', () {
+        var board = new Board();
+        board.addCellAt(2,2);
+        board.addCellAt(3,2);
+
+        var nextBoard = board.nextState();
+
+        expect(nextBoard.hasCellAt(2,2), isFalse);
+        expect(nextBoard.hasCellAt(3,2), isFalse);
+      });
+
+    });
+
+    group('2. Any live cell with more than three live neighbours dies:', () {
+
+      test('Cell with four neighbours', () {
+        var board = new Board();
+        board.addCellAt(2,2);
+        // Neighbours:
+        board.addCellAt(3,2);
+        board.addCellAt(1,2);
+        board.addCellAt(2,1);
+        board.addCellAt(2,3);
+
+        var nextBoard = board.nextState();
+
+        expect(nextBoard.hasCellAt(2,2), isFalse);
+      });
+
+    });
+
+    group('3. Any cell with two or three neighbours lives on:', () {
+
+      test('Cell with TWO neighbours', () {
+        var board = new Board();
+        board.addCellAt(2,2);
+        // Neighbours:
+        board.addCellAt(3,2);
+        board.addCellAt(1,2);
+
+        var nextBoard = board.nextState();
+
+        expect(nextBoard.hasCellAt(2,2), isTrue);
+      });
+
+      test('Cell with THREE neighbours', () {
+        var board = new Board();
+        board.addCellAt(2,2);
+        // Neighbours:
+        board.addCellAt(3,2);
+        board.addCellAt(1,2);
+        board.addCellAt(2,1);
+
+        var nextBoard = board.nextState();
+
+        expect(nextBoard.hasCellAt(2,2), isTrue);
+      }, skip: 'need to test only with two neighbours first');
+
+    });
+
+
   });
 
 }
